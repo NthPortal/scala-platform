8e9ec91e75a9ba7184c1d78fb7954637f435a083
Marissa/Princess <dev@princess.lgbt>
2023-05-20 04:47:35 -0400

    Add scaladocs

--- platform/jvm/src/main/scala/lgbt/princess/platform/Platform.scala
+++ platform/jvm/src/main/scala/lgbt/princess/platform/Platform.scala
@@ -18,8 +18,15 @@ package lgbt.princess.platform
 
 import scala.annotation.unchecked.uncheckedStable
 
+/** A utility for checking what platform Scala is running on. */
 object Platform {
-  @uncheckedStable def isJVM: Boolean    = true
-  @uncheckedStable def isJS: Boolean     = false
+
+  /** @return whether or not Scala is running on the JVM */
+  @uncheckedStable def isJVM: Boolean = true
+
+  /** @return whether or not Scala is running on JavaScript */
+  @uncheckedStable def isJS: Boolean = false
+
+  /** @return whether or not Scala is running natively */
   @uncheckedStable def isNative: Boolean = false
 }
